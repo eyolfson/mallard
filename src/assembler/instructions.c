6072ac5c98b803ac40e3a2c21603d4adc9881434
Jonathan Eyolfson <jon@eyolfson.com>
2023-05-22 20:52:35 -0400

    Added stype instruction.

--- src/assembler/instructions.c
+++ src/assembler/instructions.c
@@ -8,6 +8,15 @@ struct itype_instruction {
     int imm : 12;
 } __attribute__((packed));
 
+struct stype_ast_node {
+    unsigned int opcode : 7;
+    unsigned int imm4_0 : 5;
+    unsigned int funct3 : 3;
+    unsigned int rs1 : 5;
+    unsigned int rs2 : 5;
+    int imm11_5 : 7;
+};
+
 struct utype_instruction {
     unsigned int opcode : 7;
     unsigned int rd : 5;
