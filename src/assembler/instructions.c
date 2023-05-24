9e7c627a3e4fdb73f106bd1f519f790f02d84e84
Jonathan Eyolfson <jon@eyolfson.com>
2023-05-23 20:17:48 -0400

    Added rtype instructions.

--- src/assembler/instructions.c
+++ src/assembler/instructions.c
@@ -5,10 +5,19 @@ struct itype_instruction {
     unsigned int rd : 5;
     unsigned int funct3 : 3;
     unsigned int rs1 : 5;
-    int imm : 12;
+    int imm11_0 : 12;
 } __attribute__((packed));
 
-struct stype_ast_node {
+struct rtype_instruction {
+    unsigned int opcode : 7;
+    unsigned int rd : 5;
+    unsigned int funct3 : 3;
+    unsigned int rs1 : 5;
+    unsigned int rs2 : 5;
+    unsigned int funct7 : 7;
+};
+
+struct stype_instruction {
     unsigned int opcode : 7;
     unsigned int imm4_0 : 5;
     unsigned int funct3 : 3;
@@ -20,5 +29,24 @@ struct stype_ast_node {
 struct utype_instruction {
     unsigned int opcode : 7;
     unsigned int rd : 5;
-    int imm : 20;
+    int imm31_12 : 20;
 } __attribute__((packed));
+
+struct ujtype_instruction {
+    unsigned int opcode : 7;
+    unsigned int rd : 5;
+    unsigned int imm19_12 : 8;
+    unsigned int imm11 : 1;
+    unsigned int imm10_1 : 10;
+    int imm20 : 1;
+} __attribute__((packed));
+
+bool is_auipc_instruction(uint32_t data) {
+    struct utype_instruction i = *((struct utype_instruction*) &data);
+    return i.opcode == 0x17;
+}
+
+bool is_lui_instruction(uint32_t data) {
+    struct utype_instruction i = *((struct utype_instruction*) &data);
+    return i.opcode == 0x37;
+}
--- src/assembler/instructions.h
+++ src/assembler/instructions.h
@@ -1,4 +1,9 @@
 #ifndef MALLARD_INSTRUCTIONS_H
 #define MALLARD_INSTRUCTIONS_H
 
+#include <stdint.h>
+
+bool is_auipc_instruction(uint32_t data);
+bool is_lui_instruction(uint32_t data);
+
 #endif /* ifndef MALLARD_INSTRUCTIONS_H */
